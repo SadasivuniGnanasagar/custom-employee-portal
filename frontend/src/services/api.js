f21186754e1cdf7149792146c64b0544b9ad210e
Gnana Sagar <gnanasagar15@gmail.com>
2026-09-04 17:37:26 +0530

    https://custom-employee-portal-9brr.onrender.com/api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://custom-employee-portal-9brr.onrender.com';
+const API_URL = 'https://custom-employee-portal-9brr.onrender.com/api';
 
 const getToken = () => localStorage.getItem('token');
 
